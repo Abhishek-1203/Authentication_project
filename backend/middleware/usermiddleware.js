ee4db22f4552f2a92e97c4d8bae50716e5d7e21d
Abhishek <abhishek070312s@gmail.com>
2025-09-27 19:29:26 +0530

    Update usermiddleware.js

--- backend/middleware/usermiddleware.js
+++ backend/middleware/usermiddleware.js
@@ -3,6 +3,7 @@ import jwt from 'jsonwebtoken';
 const userAuth = async (req, res, next) => {
     try {
         const token = req.cookies.token;
+        console.log("hi");
         if (!token) {
             return res.json({ success: false, message: "Unauthorized" });
         }
